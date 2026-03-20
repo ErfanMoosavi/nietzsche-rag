f37a39c853aaff6008fae362b96ef95bc058670f
Erfan Moosavi <erfanmoosavi84@gmail.com>
2026-03-20 17:09:54 +0330

    Add configs

---
+++ app/config.py
@@ -0,0 +1,25 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    # App settings
+    app_name: str = "Nietzsche-Rag"
+    version: str = "0.1.0"
+    description: str = "A rag system based on Nietzsche's philosophy!"
+    contact: dict[str, str] = {
+        "name": "Erfan Moosavi",
+        "email": "erfanmoosavi84@gmail.com",
+    }
+    license_info: dict[str, str] = {"name": "MIT"}
+
+    # Qdrant settings
+
+    # OpenAI settings
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+        case_sensitive = False
+
+
+settings = Settings()
