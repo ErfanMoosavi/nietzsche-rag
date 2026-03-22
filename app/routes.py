7a3a65545b8376e8030c6bfd721983029a074bec
Erfan Moosavi <erfanmoosavi84@gmail.com>
2026-03-22 13:36:52 +0330

    Update schemas and routes

--- app/routes.py
+++ app/routes.py
@@ -2,25 +2,26 @@ from fastapi import APIRouter, Depends
 from qdrant_client import QdrantClient
 
 from app.dependencies import get_qdrant, get_rag
-from app.schemas import Point, RagReq, RetreiveRes, RetrieveReq
+from app.schemas import Point, RetreiveRes, RetrieveReq
 from app.services import Rag
 
 router = APIRouter()
 
 
-@router.post("/retrieval")
+@router.post("/retrieval", response_model=RetreiveRes)
 def retrieve(
     req: RetrieveReq,
     qdrant: QdrantClient = Depends(get_qdrant),
     rag: Rag = Depends(get_rag),
-):
+) -> RetreiveRes:
     response = rag.retrieve(qdrant, req.text, req.limit)
     points: list[Point] = []
     for res in response:
-        points.append(Point(text=res.payload["text"], score=res.score))
+        points.append(
+            Point(
+                text=res.payload["text"],
+                book_name=res.payload["book_name"],
+                score=res.score,
+            )
+        )
     return RetreiveRes(points=points)
-
-
-@router.post("/rag")
-def generate_answer(req: RagReq, qdrant: QdrantClient = Depends(get_qdrant)):
-    pass
--- app/schemas.py
+++ app/schemas.py
@@ -1,17 +1,27 @@
-from pydantic import BaseModel
+from typing import Literal
+
+from pydantic import BaseModel, Field
 
 
 class RetrieveReq(BaseModel):
-    text: str
-    limit: int | None = None
-
-
-class RagReq(BaseModel):
-    text: str
+    text: str = Field(..., description="The input text (query)")
+    limit: int | None = Field(
+        default=5, gt=0, lt=20, description="Number of results to return"
+    )
+    book_name: (
+        Literal[
+            "thus_spoke_zarathustra", "genealogy_of_morals", "twilight_of_the_idols"
+        ]
+        | None
+    ) = Field(
+        default=None,
+        description="The book name to search, if None, no book filter will be applied",
+    )
 
 
 class Point(BaseModel):
     text: str
+    book_name: str
     score: float
 
 
