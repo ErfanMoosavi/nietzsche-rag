f026fb85a368eb500d9f3f4360e5a5de1e077cd7
Erfan Moosavi <erfanmoosavi84@gmail.com>
2026-03-27 16:20:20 +0330

    Add get_openai function

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,14 +1,20 @@
+from openai import OpenAI
 from qdrant_client import QdrantClient
 
 from app.config import settings
 from app.services import Rag
 
-client = QdrantClient(path=settings.qdrant_path)
+qdrant_client = QdrantClient(path=settings.qdrant_path)
+openai_client = OpenAI(base_url=settings.base_url, api_key=settings.openai_api_key)
 rag = Rag()
 
 
 def get_qdrant() -> QdrantClient:
-    return client
+    return qdrant_client
+
+
+def get_openai() -> OpenAI:
+    return openai_client
 
 
 def get_rag() -> Rag:
