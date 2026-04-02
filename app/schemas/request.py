71e1a08261102e25e1b2a45702b960037aced3c6
Erfan Moosavi <erfanmoosavi84@gmail.com>
2026-04-02 19:25:30 +0330

    Update request schemas

--- app/schemas/request.py
+++ app/schemas/request.py
@@ -8,15 +8,9 @@ class RetrieveReq(BaseModel):
     limit: int = Field(
         default=5, gt=0, lt=20, description="Number of results to return"
     )
-    book: (
-        Literal[
-            "thus_spoke_zarathustra", "genealogy_of_morals", "twilight_of_the_idols"
-        ]
-        | None
-    ) = Field(
-        default=None,
-        description="The book name to search, if None, no book filter will be applied",
-    )
+    book: Literal[
+        "all", "thus_spoke_zarathustra", "genealogy_of_morals", "twilight_of_the_idols"
+    ] = Field(default="all", description="Restrict retrieval to a specific book.")
 
 
 class RagReq(BaseModel):
@@ -24,17 +18,11 @@ class RagReq(BaseModel):
         ..., example="Who is Dionysus?", description="The input question"
     )
     retrieval_limit: int = Field(
-        default=5,
+        default=3,
         gt=0,
         lt=20,
         description="Number of results to return for RAG pipeline",
     )
-    based_on: (
-        Literal[
-            "thus_spoke_zarathustra", "genealogy_of_morals", "twilight_of_the_idols"
-        ]
-        | None
-    ) = Field(
-        default=None,
-        description="Restrict retrieval to a specific book.",
-    )
+    based_on: Literal[
+        "all", "thus_spoke_zarathustra", "genealogy_of_morals", "twilight_of_the_idols"
+    ] = Field(default="all", description="Restrict retrieval to a specific book.")
