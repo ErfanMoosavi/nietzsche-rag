eddb220f28ab8fa40e8a271f0050255bd9d318de
Erfan Moosavi <erfanmoosavi84@gmail.com>
2026-04-02 16:01:07 +0330

    Simplify embed function

--- app/utils/embed.py
+++ app/utils/embed.py
@@ -3,5 +3,4 @@ from sentence_transformers import SentenceTransformer
 
 def embed(text: str) -> list[float]:
     model = SentenceTransformer("all-MiniLM-L6-v2")
-    embedding = model.encode(text).tolist()
-    return embedding
+    return model.encode(text).tolist()
