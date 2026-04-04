146ce193c9ccbcc7b807c056d33c47300571a79a
Erfan Moosavi <erfanmoosavi84@gmail.com>
2026-04-04 21:27:22 +0330

    fix: small changes

--- app/setup/setup.py
+++ app/setup/setup.py
@@ -4,8 +4,9 @@ from llama_index.core.node_parser import SentenceSplitter
 from qdrant_client import QdrantClient, models
 from sentence_transformers import SentenceTransformer
 
+from app.config import settings
 from app.dependencies import get_qdrant
-from app.setup.config import setup_config
+from app.setup.setup_config import setup_config
 
 
 def _is_model_present() -> bool:
@@ -14,7 +15,7 @@ def _is_model_present() -> bool:
 
 def _is_collection_populated(qdrant_client: QdrantClient) -> bool:
     try:
-        count = qdrant_client.count(collection_name=setup_config.COLLECTION_NAME)
+        count = qdrant_client.count(collection_name=setup_config.collection_name)
         return count.count > 0
     except Exception:
         return False
@@ -22,18 +23,18 @@ def _is_collection_populated(qdrant_client: QdrantClient) -> bool:
 
 def _create_collection(qdrant_client: QdrantClient) -> None:
     """Creates the Qdrant collection if it doesn't exist"""
-    if qdrant_client.collection_exists(collection_name=setup_config.COLLECTION_NAME):
+    if qdrant_client.collection_exists(collection_name=setup_config.collection_name):
         return
     else:
         qdrant_client.create_collection(
-            collection_name=setup_config.COLLECTION_NAME,
+            collection_name=setup_config.collection_name,
             vectors_config={
                 "dense": models.VectorParams(
-                    size=setup_config.VECTOR_SIZE, distance=models.Distance.COSINE
+                    size=setup_config.vector_size, distance=models.Distance.COSINE
                 )
             },
             hnsw_config=models.HnswConfigDiff(
-                m=setup_config.HNSW_M, ef_construct=setup_config.HNSW_EF_CONSTRUCT
+                m=setup_config.hnsw_m, ef_construct=setup_config.hnsw_ef_construct
             ),
         )
 
@@ -41,7 +42,7 @@ def _create_collection(qdrant_client: QdrantClient) -> None:
 def _create_payload_index(qdrant_client: QdrantClient) -> None:
     """Indexed the payload for more efficient search"""
     qdrant_client.create_payload_index(
-        collection_name=setup_config.COLLECTION_NAME, field_name="book"
+        collection_name=setup_config.collection_name, field_name="book"
     )
 
 
@@ -63,7 +64,7 @@ def _preprocess(text: str) -> str:
 def _chunk(text: str) -> list[str]:
     """Chunk the text using sentence chunking method"""
     splitter = SentenceSplitter(
-        chunk_size=setup_config.CHUNK_SIZE, chunk_overlap=setup_config.CHUNK_OVERLAP
+        chunk_size=setup_config.chunk_size, chunk_overlap=setup_config.chunk_overlap
     )
     return splitter.split_text(text=text)
 
@@ -114,7 +115,7 @@ def _upsert_points(
     qdrant_client: QdrantClient, points: list[models.PointStruct]
 ) -> None:
     """Upserts points to qdrant"""
-    qdrant_client.upsert(collection_name=setup_config.COLLECTION_NAME, points=points)
+    qdrant_client.upsert(collection_name=setup_config.collection_name, points=points)
 
 
 def setup() -> None:
@@ -142,7 +143,7 @@ def setup() -> None:
         model = SentenceTransformer("all-MiniLM-L6-v2", local_files_only=True)
 
         # Process each book
-        for book in setup_config.BOOKS:
+        for book in settings.books.keys():
             print(f"Processing: {book}")
 
             book_file = book + ".txt"
