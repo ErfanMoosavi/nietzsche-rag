35b4270f8b25ea843690e7c8f989f120dca1f6ad
Erfan Moosavi <erfanmoosavi84@gmail.com>
2026-04-04 22:00:35 +0330

    Change hnsw config

--- app/setup/setup_config.py
+++ app/setup/setup_config.py
@@ -4,9 +4,9 @@ class SetupConfig:
     vector_size = 384
 
     hnsw_m = 32
-    hnsw_ef_construct = 300
+    hnsw_ef_construct = 200
 
-    chunk_size = 150
+    chunk_size = 160
     chunk_overlap = 35
 
 
